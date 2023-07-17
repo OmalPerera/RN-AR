a1673f3b72e38c501b75f254450c2456b62fb315
Omal <omal@ascentic.se>
2023-07-17 10:21:37 +0530

    add 3D object in scene

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import {StyleSheet} from 'react-native';
+import {
+  ViroARScene,
+  ViroText,
+  ViroConstants,
+  ViroARSceneNavigator,
+} from '@viro-community/react-viro';
+import VIRComp from './src/ViroScene.component';
+
+const HelloWorldSceneAR = () => {
+  const [text, setText] = useState('Initializing AR...');
+
+  function onInitialized(state, reason) {
+    console.log('guncelleme', state, reason);
+    if (state === 3) {
+      setText('Hello World!');
+    } else if (state === 1) {
+      // Handle loss of tracking
+    }
+  }
+
+  return <VIRComp onInitialized={onInitialized} />;
+};
+
+export default () => {
+  return (
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: HelloWorldSceneAR,
+      }}
+      style={styles.f1}
+    />
+  );
+};
+
+var styles = StyleSheet.create({
+  f1: {flex: 1},
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
---
+++ src/components/V3DObjects.tsx
@@ -0,0 +1,32 @@
+import {Viro3DObject, ViroNode} from '@viro-community/react-viro';
+import React, {useState} from 'react';
+import {StyleSheet} from 'react-native';
+
+const Simple3DObject = props => {
+  const {object} = props;
+
+  console.log('----L : ', object);
+
+  /* This ViroNode is used as a parent for 3 children - 3D Object, Particle Emitters (if present in data model) and shadows. 
+                This is done so that all three children are positioned in relation to each other in the scene graph, and any touch events
+                that change their position of the object, affect all three */
+
+  return (
+    <ViroNode position={object.position}>
+      <Viro3DObject
+        source={object.obj}
+        resources={object.resources}
+        position={object.position}
+        scale={object.scale}
+        type={object.type}
+        animation={{...object.animation, run: true}}
+        //materials={['heart']}
+        //dragType="FixedDistance"
+        onDrag={() => {}}
+      />
+    </ViroNode>
+  );
+};
+export default Simple3DObject;
+
+var styles = StyleSheet.create({});
